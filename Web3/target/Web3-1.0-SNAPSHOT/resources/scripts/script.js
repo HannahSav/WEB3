da68b527490c8348942fee87520b54d1a8113de1
Oleg <kurashov-ole@yandex.ru>
2020-12-08 17:55:38 +0300

    index made

---
+++ Web3/target/Web3-1.0-SNAPSHOT/resources/scripts/script.js
@@ -0,0 +1,56 @@
+function XYR(){
+    x = $("input[name='form:slider_x_hidden']").val();
+    y = $("input[name='form:y_in']").val();
+    r = $("input[id='form:r_value']").val();
+}
+
+function checkY(x, y) {
+    if (!y) {
+        return exceptionY('<br>Вы не ввели Y</br>')
+    } else if (isNaN(y)) {
+        return exceptionY('<br>Y должен быть числом</br>')
+    } else if (y < -3 || y > 3) {
+        return exceptionY('<br>Y не принадлежит [-3:3]</br>')
+    } else {
+        $(".exceptionY").html('');
+        return true
+    }
+}
+
+function exceptionY(message) {
+    $(".exceptionY").html(message);
+    point(0,0, 1);
+    return false
+}
+
+$(function () {
+    // $('#send').on('click', function (event) {
+    // XYR();
+    // if(checkY(x, y, r)) {
+    //     $.get(
+    //     "/CheckArea",
+    //     {x: x, y: y, r: r}
+    //     )
+    //     // drawPoints(x, y, r)
+    // } else {
+    //     event.preventDefault()
+    //     }
+    // });
+
+});
+
+function change() {
+    setTimeout(function(){
+        XYR();
+        if (checkY(x,y))
+            point(x, y, r);
+    }, 150);
+
+}
+
+
+function point(x, y, r) {
+    $('#point').attr("cx", (x * 120 / r + 200))
+        .attr("cy", (y * -120 / r + 200));
+}
+
