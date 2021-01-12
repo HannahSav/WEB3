015cec3f290f8d1d79deec8f04a2df091572cd26
Oleg <kurashov-ole@yandex.ru>
2021-01-12 13:25:39 +0300

    Too Too
    -сss
    -целые х
    -обновления карты
    -расширить картинку

--- lab3/src/main/java/PointDao.java
+++ lab3/src/main/java/PointDao.java
@@ -7,7 +7,7 @@ public class PointDao {
 
     public void initConnection() throws Exception {
         try {
-            connection = DriverManager.getConnection("jdbc:postgresql://pg:5432/studs", "KorolIShut", "xxxxx");
+            connection = DriverManager.getConnection("jdbc:postgresql://pg:5432/studs", "Korol I Shut", "xxxxx");
             connection.createStatement().execute("create table if not exists points286536(x float, y float, r float, result char(10))");
         } catch (SQLException e) {throw new Exception("Couldn't create connection");}
     }
